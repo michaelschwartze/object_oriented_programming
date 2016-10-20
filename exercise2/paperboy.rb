0bb6a7380e648378b24a86be45bc1a8b95203cad
Michael Schwartze <mschwartze@gmail.com>
2016-10-20 16:26:50 -0400

    Add deliver method

--- exercise2/paperboy.rb
+++ exercise2/paperboy.rb
@@ -1,7 +1,7 @@
 class Paperboy
 
   attr_reader :name
-  attr_accessor :experience, :quota
+  attr_accessor :experience
 
   def initialize(name)
     @name = name
@@ -13,6 +13,24 @@ class Paperboy
     quota = 50 + (@experience/2)
   end
 
+  def deliver(start_address, end_address)
+    papers_delivered = end_address - start_address
+    per_paper_pay_total = 0.25 * papers_delivered
+
+    if papers_delivered > quota
+      money_earned = per_paper_pay_total + ( (papers_delivered - quota) * 0.25 )
+    elsif papers_delivered < quota
+      money_earned = per_paper_pay_total - 2
+    elsif papers_delivered == quota
+      money_earned = per_paper_pay_total
+    end
+
+    @experience = @experience + papers_delivered
+    @earnings = @earnings + money_earned
+    return money_earned
+
+  end
+
 end
 
-timmy = Paperboy.new('Timmy')
+johnny = Paperboy.new('Johnny')
