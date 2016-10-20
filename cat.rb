e9e0c166e536bafd59df4abe75f0aa6179645041
Michael Schwartze <mschwartze@gmail.com>
2016-10-20 14:18:14 -0400

    Create Meowsers and Trousers

--- cat.rb
+++ cat.rb
@@ -1,5 +1,10 @@
 class Cat
   def initialize(name, preferred_food, mealtime)
-
+    @name = name
+    @preferred_food = preferred_food
+    @mealtime = mealtime
   end
 end
+
+meowsers = Cat.new('Meowsers', 'Fancy Feast', 19)
+trousers = Cat.new('Trousers', 'Kibbles n Bits', 18)
