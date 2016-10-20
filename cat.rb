d867a9d380939a4f9f5fc8c63059980bca9dd579
Michael Schwartze <mschwartze@gmail.com>
2016-10-20 14:34:58 -0400

    Add eats_at class method

--- cat.rb
+++ cat.rb
@@ -1,9 +1,24 @@
 class Cat
+  
+  attr_reader :mealtime
+
   def initialize(name, preferred_food, mealtime)
     @name = name
     @preferred_food = preferred_food
     @mealtime = mealtime
   end
+
+  def eats_at
+    if @mealtime > 12
+      am_pm = "PM"
+      mealtime = @mealtime - 12
+    else
+      am_pm = "AM"
+      mealtime = @mealtime
+    end
+    "#{mealtime} #{am_pm}"
+  end
+
 end
 
 meowsers = Cat.new('Meowsers', 'Fancy Feast', 19)
