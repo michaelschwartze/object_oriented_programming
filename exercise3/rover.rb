00fc1a98819d63644a9f47724cb4fe18099844a8
Michael Schwartze <mschwartze@gmail.com>
2016-10-20 20:55:00 -0400

    Move read_instruction out of class module, add move method

--- exercise3/rover.rb
+++ exercise3/rover.rb
@@ -1,11 +1,13 @@
 class Rover
 
   attr_accessor :x_coord, :y_coord, :direction
+  attr_reader :direction_to_degrees
 
   def initialize(x_coord, y_coord, direction)
     @x_coord    = x_coord
     @y_coord    = y_coord
     @direction  = direction
+    @direction_to_degrees = direction_to_degrees
   end
 
   def direction_to_degrees
@@ -20,25 +22,34 @@ class Rover
     end
   end
 
-  def read_instruction
-    #5, 5 -> input 1, grid size
-    #1, 1, N -> input 2,
-    #LRLMLRRLLMMMMR -> input 3
-  end
-
   def rover_position
     "Rover is located at (x, y) coordinates #{ @x_coord }, #{ @y_coord } and is pointing #{ @direction }"
   end
 
   def move
-    #x right left
-    #return "You want to move."
+    if @direction == "N"
+      @y_coord = @y_coord + 1
+    elsif @direction == "E"
+      @x_coord = @x_coord + 1
+    elsif @direction == "S"
+      @y_coord = @y_coord - 1
+    elsif @direction == "W"
+      @x_coord = @x_coord - 1
+    end
   end
 
   def turn
-    return "You want to turn."
+
   end
 
 end
 
+def read_instruction
+  puts 'Enter grid size as "x y":'
+  grid_size = gets.chomp
+  puts grid_size.chars[0]
+  puts grid_size.chars[2]
+end
+
 rover1 = Rover.new(1, 2, "N")
+#rover2 = Rover.new()
