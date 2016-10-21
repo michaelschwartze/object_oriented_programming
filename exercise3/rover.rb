ce4fff4184aff62c0cac56f886b2de19507b6d72
Michael Schwartze <mschwartze@gmail.com>
2016-10-20 21:43:49 -0400

    Add COMPASS_HASH and turn instance function, remove direction_to_degrees function

--- exercise3/rover.rb
+++ exercise3/rover.rb
@@ -1,28 +1,32 @@
 class Rover
 
   attr_accessor :x_coord, :y_coord, :direction
-  attr_reader :direction_to_degrees
+
+  COMPASS_HASH = { "N" => 0, "E" => 90, "S" => 180, "W" => 270 }
 
   def initialize(x_coord, y_coord, direction)
     @x_coord    = x_coord
     @y_coord    = y_coord
     @direction  = direction
-    @direction_to_degrees = direction_to_degrees
   end
 
-  def direction_to_degrees
-    if @direction == "N"
-      @direction_to_degrees = 0
-    elsif @direction == "E"
-      @direction_to_degrees = 90
-    elsif @direction == "S"
-      @direction_to_degrees = 180
-    else
-      @direction_to_degrees = 270
-    end
+  def dir_to_deg
+    COMPASS_HASH[@direction]
   end
 
-  def rover_position
+  # def direction_to_degrees DELETE
+  #   if @direction == "N"
+  #     @direction_to_degrees = 0
+  #   elsif @direction == "E"
+  #     @direction_to_degrees = 90
+  #   elsif @direction == "S"
+  #     @direction_to_degrees = 180
+  #   else
+  #     @direction_to_degrees = 270
+  #   end
+  # end
+
+  def position
     "Rover is located at (x, y) coordinates #{ @x_coord }, #{ @y_coord } and is pointing #{ @direction }"
   end
 
@@ -38,8 +42,12 @@ class Rover
     end
   end
 
-  def turn
-
+  def turn(direction)
+    if direction == "R"
+      @direction = COMPASS_HASH.key(COMPASS_HASH[@direction] + 90)
+    elsif direction == "L"
+      @direction = COMPASS_HASH.key(COMPASS_HASH[@direction] - 90)
+    end
   end
 
 end
@@ -51,5 +59,5 @@ def read_instruction
   puts grid_size.chars[2]
 end
 
-rover1 = Rover.new(1, 2, "N")
+rover1 = Rover.new(1, 2, "E")
 #rover2 = Rover.new()
